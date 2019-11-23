f559c74bcb6379e482bf6d6682bacc83cf16be13
Michał <michal.martyniuk7@gmail.com>
2019-11-23 14:49:38 +0100

    initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,17 @@
 import { hot } from "react-hot-loader/root";
 import React from "react";
-import ReactDOM from "react-dom";
-import App from './App';
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import App from "./components/App";
 
+const store = createStore(rootReducer);
 const HotApp = hot(App);
 
-ReactDOM.render(<HotApp />, document.getElementById("root"));
+render(
+  <Provider store={store}>
+    <HotApp />
+  </Provider>,
+  document.getElementById("root")
+);
